52922859e0d62c1ad07b74b758318c065a666315
aimeemoffat <aimeemoffat@labpc014.mshome.net>
2018-01-24 09:30:50 +0000

    Initial Commit

---
+++ src/lab2/MyClassOne.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author aimeemoffat
+ */
+
+
+public class MyClassOne {
+    
+    public void MethodOne()
+{
+    System.out.println("Calling this from MyClassOne");
+}
+    
+    public int AddNumber(int num1, int num2)
+    {
+        int result = num1 + num2;
+        return result;
+    }
+    
+}

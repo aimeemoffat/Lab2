7b4e47568eff49d9f22ebf2bad7acdb2942bbbd1
aimeemoffat <aimeemoffat@labpc014.mshome.net>
2018-01-24 09:31:19 +0000

    Initial Commit

---
+++ src/MyPackage/PackageClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyPackage;
+
+/**
+ *
+ * @author aimeemoffat
+ */
+public class PackageClass {
+    
+    public String PackageMethod()
+    {
+        String text = "This class is in a new package";
+        
+        return text;
+    }
+    
+}
